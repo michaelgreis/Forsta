b068fef5d1dd2b9d0506e15d9d24114c095105f3
michaelgreis <michaelgreis@gmail.com>
2016-12-07 19:38:18 -0600

    Added dev space and pandas dataframe beginning

--- Modules/text_parser.py
+++ Modules/text_parser.py
@@ -1,6 +1,13 @@
 import os #needed for the looping through of the logs
 import time # needed for pausing, to make sure program is working alright when debugging.
+import pandas as pd #needed for the dataframe read in
+import numpy
+import matplotlib.pyplot
+
+
 import string
+
+
 
 path = '../Sample_Data_Generation/Sample_Data/'
 input_lines=[]
@@ -16,7 +23,7 @@ for filename in os.listdir(path):
 	file.close()
 	print(filename + ' has been written into array successfully.')
 
-time.sleep(5) #only used for debugging purposes. Want to make sure execution is occurring correctly.
+time.sleep(1) #only used for debugging purposes. Want to make sure execution is occurring correctly.
 
 for index, values in enumerate(input_lines):
 	#print(values) #I know this was occurring correctly, so no longer need it
@@ -24,6 +31,26 @@ for index, values in enumerate(input_lines):
 	for word in values.split(): #splitting by the space
 		for specific_word in word.split('='): #splitting by equal sign
 			input_words.append([index,specific_word])
-	print(input_words)
+	#print(input_words) No longer needed
+
+#print([i[0] for i in input_words])
+#print([i[1] for i in input_words])
+
+#this is the start of the use of Pandas
+
+#df_input_words = pd.Series(input_words)
+df_input_words = pd.Series([i[1] for i in input_words], index=[i[0] for i in input_words])
+#df_input_words.groupby(df_input_words).count()
+
+#print(df_input_words)
+
+#df = pd.DataFrame(input_words,columns=headers)
 
 
+
+#outputting to a test file
+f_test = open('../Dev_Space/testing.txt','w')
+test_data = input_words#df_input_words.groupby([i[1] for i in input_words]).count()
+f_test.write(str(test_data))
+f_test.close()
+#end of test file output
