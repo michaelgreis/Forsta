8b4ae5ab525a761b5ab5fe08a67952b616636bc1
Dan Veenstra <danielveenstra11@gmail.com>
2017-01-04 18:54:08 -0600

    Dumping attributes of parsed statements for further extraction

--- parser/forstaparser.py
+++ parser/forstaparser.py
@@ -4,4 +4,18 @@ class ForstaParser():
 
 	def parse(self,query):
 		query_record = {}
+		parseResults = sp.select_stmt.parseString(query)
+		# print(parseResults)
+		# print(parseResults.dump())
+		# print(type(parseResults["from"]))
+		print(parseResults.items)
+		self.get_tables(parseResults)
 		return query_record
+
+	def get_tables(self,parseResults):
+		from_clause = parseResults["from"]
+		print(from_clause.items)
+		for x in from_clause:
+			
+			print(x)
+			print(type(x))
