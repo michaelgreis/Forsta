ff7a473138fa92f65e44da2af598003c97b488f4
michaelgreis <michaelgreis@gmail.com>
2017-02-12 16:53:21 -0600

    Relational Algorithm Started
    Started doing the rleational algorithm. Have the reading and pivoting of
    the dataframe, but don't have the relationships defined yet.

--- Testing_Forsta_Parser_Relative.py
+++ Testing_Forsta_Parser_Relative.py
@@ -1,4 +1,6 @@
 from classes import forstaparser as fp
+from classes import forstaalgorithm as fa
+
 print('LOADED BRO')
 
 # this is used to define the cleaning values we are looking for.
@@ -16,4 +18,13 @@ print('Logs Cleaned.')
 
 dataframe = fp.parser.reading_into_dataframe(logs_input)
 
-print(dataframe)
+
+dataframe_count = fa.word2vec.wordcount(dataframe)
+
+#print(dataframe_count)
+
+dataframe_pivot = fa.word2vec.pivot_data(dataframe_count)
+
+print(dataframe_pivot)
+
+fa.word2vec.word_relationship(dataframe_pivot)
