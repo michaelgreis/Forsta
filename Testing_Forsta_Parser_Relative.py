86b6b533ad51559d53322e4c2f597053e01c640a
michaelgreis <michaelgreis@gmail.com>
2017-02-12 13:18:17 -0600

    All Classes Tested
    All of the classes for the Forsta parser have been tested and
    authenticated.

--- Testing_Forsta_Parser_Relative.py
+++ Testing_Forsta_Parser_Relative.py
@@ -1,6 +1,19 @@
 from classes import forstaparser as fp
 print('LOADED BRO')
 
+# this is used to define the cleaning values we are looking for.
+cleaning_values = []
+
+cleaning_values.append('select')
+
 logs_input = fp.parser.read_logs('Sample_Data_Generation/Sample_Data/')
 
-print(logs_input)
+print('Logs Read.')
+
+logs_input = fp.parser.clean_input_list(cleaning_values,logs_input)
+
+print('Logs Cleaned.')
+
+dataframe = fp.parser.reading_into_dataframe(logs_input)
+
+print(dataframe)
--- classes/forstaparser.py
+++ classes/forstaparser.py
@@ -21,15 +21,17 @@ class parser():
 	#This function accepts a list of strings (cleaning_values) to filter out irrelevant data from another list of strings (list_to_clean)
 	# Specifically, you pass the values you WANT to include
 	def clean_input_list(cleaning_values,list_to_clean):
+		total_line_count = 0
+		clean_count = 0
 		clean_list = [] #this is the output list
 		for line in list_to_clean: #iterating through the lines
 			for sterilization_values in cleaning_values: # iterating through the cleaning list
-				if line.str.find(sterilization_values,beg=0,end=len(line))>=0: #this checks if there is a sterilization value in the string.
+				if line.upper().find(sterilization_values.upper())>=0: #this checks if there is a sterilization value in the string.
 					clean_list.append(line)
 					clean_count+=1 # count of lines cleaned
 					break # if one of the values is found, it returns the line.
 			total_line_count+=1 #count of total lines read
-		print('List of ' + total_line_count + ' has been cleaned to ' + clean_count + ' for lines containing the following values: ' + cleaning_values) # outputs log
+		print('List of ' + str(total_line_count) + ' has been cleaned to ' + str(clean_count) + ' for lines containing the following values: ' + str(cleaning_values)) # outputs log
 		return clean_list
 
 	def reading_into_dataframe(input_array):
@@ -37,6 +39,6 @@ class parser():
 		for index, values in enumerate(input_array):
 			for word in values.split(): #splitting by the space
 				for specific_word in word.split('='): #splitting by equal sign
-					input_word.append([index,specific_word]) #appending to the input word with the index for the word
+					input_words.append([index,specific_word]) #appending to the input word with the index for the word
 		df_input_words = pd.DataFrame(input_words,columns=list(['log_entry', 'word']))
 		return(df_input_words)
