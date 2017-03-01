8d5c4bf7e303ac2e96005cf4ce579ab02c84b400
michaelgreis <michaelgreis@gmail.com>
2017-03-01 08:29:43 -0600

    Created Some More Sample Data. Refined the clean_input_list function in class parser.

--- Testing_Forsta_Parser_Relative.py
+++ Testing_Forsta_Parser_Relative.py
@@ -1,30 +1,38 @@
 from classes import forstaparser as fp
-from classes import forstaalgorithm as fa
+#from classes import forstaalgorithm as fa
 
 print('LOADED BRO')
 
 # this is used to define the cleaning values we are looking for.
 cleaning_values = []
-
 cleaning_values.append('select')
+cleaning_values.append(':LOG:  statement: ')
+
+#this defines the split value that is used in clean_input_list
+sql_log_split = ':LOG:  statement: '
+
 
 logs_input = fp.parser.read_logs('Sample_Data_Generation/Sample_Data/')
 
+#print(logs_input)
+
 print('Logs Read.')
 
-logs_input = fp.parser.clean_input_list(cleaning_values,logs_input)
+logs_input = fp.parser.clean_input_list(cleaning_values,logs_input,sql_log_split)
 
 print('Logs Cleaned.')
 
-dataframe = fp.parser.reading_into_dataframe(logs_input)
+print(logs_input)
+
+#dataframe = fp.parser.reading_into_dataframe(logs_input)
 
 
-dataframe_count = fa.word2vec.wordcount(dataframe)
+#dataframe_count = fa.word2vec.wordcount(dataframe)
 
 #print(dataframe_count)
 
-dataframe_pivot = fa.word2vec.pivot_data(dataframe_count)
+#dataframe_pivot = fa.word2vec.pivot_data(dataframe_count)
 
-print(dataframe_pivot)
+#print(dataframe_pivot)
 
-fa.word2vec.word_relationship(dataframe_pivot)
+#fa.word2vec.word_relationship(dataframe_pivot)
