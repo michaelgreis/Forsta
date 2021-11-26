88369f7bd7caea304216af42b24de23fa41de519
Michael Greis <michaelgreis@gmail.com>
2021-11-26 10:57:47 -0600

    Updated Lambda Function for ingestion
    now calls the ingestion piece of the process to automatically load the S3 files to the consolidated "Landing" bucket.

--- Parser/lambda_function.py
+++ Parser/lambda_function.py
@@ -1,5 +1,11 @@
-from tests import test_parser
+#from tests import test_parser
+from classes import ingester
 
 def lambda_handler(event, context):
-    test_parser.t_parser()
+    bucket_name = event['Records'][0]['s3']['bucket']['name']
+    target_bucket = 'landing-bucket-11.15.2021'
+
+    landing_ingester = ingester.ingester()
+    landing_ingester.copy_bucket(bucket_name,target_bucket)
+    #test_parser.t_parser()
     print("Completed")
